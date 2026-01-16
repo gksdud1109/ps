f7193072be8f7b9c4eec9a343026aa2e8e401b89
gksdud1109 <vectorh532@gmail.com>
2026-01-16 21:24:57 +0900

    example: list 예제 코드

---
+++ 2026/0x04_linked_list/list_example.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+  list<int> L = {1, 2};
+  list<int>::iterator t = L.begin();
+
+  L.push_front(10); // 10 1 2
+
+  cout << *t << '\n'; // t가 가리키는 값 = 1을 출력
+
+  L.push_back(5); // 10 1 2 5
+
+  L.insert(t, 6); // t가 가리키는 곳 앞에 6을 삽입, 10 6 1 2 5
+  t++;
+  t = L.erase(t); // t가 가리키는 값을 제거, 그다음 원소인 5의 위치를 반환
+  // 10 6 1 5, t가 가리키는 값은 5
+
+  cout << *t << '\n'; // 5
+
+  for(auto i : L)
+    cout << i << ' ';
+  cout << '\n';
+  for(list<int>::iterator it = L.begin(); it != L.end(); it++)
+    cout << *it << ' ';
+}

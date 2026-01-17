ff8e1c0a87ce0b59db53915982f3b69d68679dc2
gksdud1109 <vectorh532@gmail.com>
2026-01-17 11:14:43 +0900

    example: stack stl

---
+++ 2026/0x05_stack/stack_example.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+  stack<int> S;
+  S.push(10);
+  S.push(20);
+  S.push(30);
+
+  cout << S.size() << '\n';
+  if(S.empty()) cout << "S is empty\n";
+  else cout << "S is not empty\n";
+
+  S.pop();
+  cout << S.top() << '\n';
+  S.pop();
+  cout << S.top() << '\n';
+  S.pop();
+  if(S.empty()) cout << "S is empty\n";
+  else cout << "S is not empty\n";
+}

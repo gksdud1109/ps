61b16c18ff4bd439e088667d062b0b01267cefa9
0one_zer0 <vectorh532@gmail.com>
2026-02-02 12:13:04 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 0 ms, Memory: 2544 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
@@ -1,40 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int LMT = 100001;
-int board[LMT+2];
-int sis, bro;
-queue<int> Q;
-void teleport(int num) {
-  int tmp = num;
-  if (!tmp) return;
-  while (tmp < LMT && !board[bro]) {
-    if (!board[tmp]) {
-      board[tmp] = board[num];
-      Q.push(tmp);
-      if (tmp == bro) return;
-    }
-    tmp <<= 1;
-  }
-}
-int main(void) {
+const int MX = 100000;
+const int INF = 1e9;
+int board[MX+2];
+
+int main(){
   ios::sync_with_stdio(0);
   cin.tie(0);
 
-  cin >> sis >> bro;
-  board[sis] = 1;
-  Q.push(sis);
-  teleport(sis);
-  while (!board[bro]) {
-    int v = Q.front(); Q.pop();
-    vector<int> nvLst = { v + 1, v - 1 };
-    for (int nv : nvLst) {
-      if (nv < 0 || LMT <= nv) continue;
-      if (board[nv]) continue;
-      board[nv] = board[v] + 1;
-      Q.push(nv);
-      teleport(nv);
+  int N, K; cin >> N >> K;
+  fill(board, board+MX+1, -1);
+
+  deque<int> DQ;
+  DQ.push_back(N);
+  board[N] = 0;
+
+  while(!DQ.empty()){
+    int cur = DQ.front(); DQ.pop_front();
+    if(cur == K) break;
+
+    int nx = cur*2;
+    if(nx <= MX && board[nx] == -1){
+      board[nx] = board[cur];
+      DQ.push_front(nx);
+    }
+
+    for(int dir : {cur-1,cur+1}){
+      if(dir < 0 || dir > MX) continue;
+      if(board[dir] != -1) continue;
+      board[dir] = board[cur]+1;
+      DQ.push_back(dir);
     }
   }
-  cout << board[bro]-1;
+
+  cout << board[K];
 }

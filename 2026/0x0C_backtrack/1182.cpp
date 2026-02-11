0af653a37425e1c58f74c4eee707ee9602d1f068
gksdud1109 <vectorh532@gmail.com>
2026-02-11 12:56:26 +0900

    연습문제 1182-부분수열의 합

---
+++ 2026/0x0C_backtrack/1182.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, S;
+int arr[30];
+int cnt=0;
+void func(int cur, int tot){
+  if(cur == N){
+    if(tot == S) cnt++;
+    return;
+  }
+
+  func(cur+1, tot);
+  func(cur+1, tot+arr[cur]);
+}
+
+int main(void){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cin >> N >> S;
+
+  for(int i=0; i<N; i++)
+    cin >> arr[i];
+
+  func(0,0);
+  if(S==0) cnt--;
+  cout << cnt;
+}

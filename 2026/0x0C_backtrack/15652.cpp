9518ebc924b13e6a609c76f24fccd3818c2657e8
gksdud1109 <vectorh532@gmail.com>
2026-02-12 10:34:11 +0900

    solve: 15652-N과M 4

---
+++ 2026/0x0C_backtrack/15652.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, M;
+int arr[10];
+
+void func(int k){
+  if(k==M){
+    for(int i=0; i<M; i++)
+      cout << arr[i] << ' ';
+    cout << '\n';
+    return;
+  }
+
+  int st = 1;
+  if(k != 0) st = arr[k-1];
+  for(int i=st; i<=N; i++){
+    arr[k] = i;
+    func(k+1);
+  }
+}
+
+int main(){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> N >> M;
+  func(0);
+}

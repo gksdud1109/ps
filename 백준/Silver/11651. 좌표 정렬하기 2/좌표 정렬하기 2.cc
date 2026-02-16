3be000d9ba048a143db0533d69b0ccc0c4e8c245
0one_zer0 <vectorh532@gmail.com>
2026-02-16 21:14:28 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 40 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+pair<int,int> p[100004];
+
+int main(){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int N; cin >> N;
+  for(int i=0; i<N; i++)
+    cin >> p[i].second >> p[i].first;
+
+  sort(p, p+N);
+  for(int i=0; i<N; i++)
+    cout << p[i].second << ' ' << p[i].first << '\n';
+}

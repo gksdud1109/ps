b6faf5305c221edb7a68a03547df99e8375590a4
0one_zer0 <vectorh532@gmail.com>
2026-02-17 20:37:41 +0900

    [Silver V] Title: 나이순 정렬, Time: 40 ms, Memory: 9840 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
@@ -1,16 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int N;
+
 int main(){
   ios::sync_with_stdio(0);
   cin.tie(0);
 
-  int N; cin >> N;
-  pair<int, string> x[N];
-
-  for(auto& s: x) cin >> s.first >> s.second;
-  stable_sort(x, x+N, [&](pair<int,string> a, pair<int,string> b){
-    return a.first < b.first;
-  });
-  for(auto p:x) cout << p.first << ' ' << p.second << '\n';
+  cin >> N;
+  vector<tuple<int, int, string>> members;
+  for(int i=0; i<N; i++){
+    int age;
+    string name;
+    cin >> age >> name;
+    members.push_back({age, i, name}); // 순서정보 i로 sort()에서도 순서 보장
+  }
+  sort(members.begin(), members.end());
+  for(auto [age, _, name] : members)
+    cout << age << " " << name << '\n';
 }

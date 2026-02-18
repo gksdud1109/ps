caef7511a5dfc08569870cb10d617cdba1e13f98
0one_zer0 <vectorh532@gmail.com>
2026-02-18 18:58:41 +0900

    [Silver II] Title: 연속합, Time: 4 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.cc
+++ 백준/Silver/1912. 연속합/연속합.cc
@@ -3,7 +3,6 @@ using namespace std;
 
 int N;
 int a[100005];
-int D[100005]; // D[i] = i번째 항으로 끝나는 연속합 중 최댓값
 
 int main(){
   ios::sync_with_stdio(0);
@@ -12,9 +11,12 @@ int main(){
   cin >> N;
   for(int i=1; i<=N; i++)
     cin >> a[i];
+  
+  int currentSum = 0, maxSum = -1001;
+  for(int i = 1; i <= N; i++) {
+    currentSum = max(0, currentSum) + a[i];
+    maxSum = max(maxSum, currentSum);
+  }
 
-  for(int i=1; i<=N; i++)
-    D[i] = max(0, D[i-1]) + a[i]; // 연속을 끊을지, 새로갈지
-
-  cout << *max_element(D+1, D+N+1);
+  cout << maxSum;
 }

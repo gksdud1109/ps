af484a8c895809f7f67c39f2fc347d364d97c926
0one_zer0 <vectorh532@gmail.com>
2026-02-18 11:32:25 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int D[1000005];
+int N;
+
+int main(){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cin >> N;
+  D[1] = 0;
+
+  for(int i=2; i<=N; i++){
+    D[i] = D[i-1] + 1;
+    if(i%2 == 0) D[i] = min(D[i], D[i/2]+1);
+    if(i%3 == 0) D[i] = min(D[i], D[i/3]+1);
+  }
+  cout << D[N];
+}

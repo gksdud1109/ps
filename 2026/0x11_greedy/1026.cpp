368f57cdfb0f6612aeafddff633881facb942de6
gksdud1109 <vectorh532@gmail.com>
2026-02-21 15:45:23 +0900

    연습문제: 1026-보물

---
+++ 2026/0x11_greedy/1026.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a[105], b[105];
+int N;
+
+int main(){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> N;
+  for(int i=0; i<N; i++) cin >> a[i];
+  for(int i=0; i<N; i++) cin >> b[i];
+
+  sort(a, a+N);
+  sort(b, b+N);
+
+  int ans = 0;
+
+  for(int i=0; i<N; i++)
+    ans += a[i] * b[N-1-i];
+  cout << ans;
+}

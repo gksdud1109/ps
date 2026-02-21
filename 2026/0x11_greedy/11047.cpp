38b0a261f2421809dd965c5c0b5b0a03e28847c7
gksdud1109 <vectorh532@gmail.com>
2026-02-21 15:10:04 +0900

    연습문제: 11047-동전0

---
+++ 2026/0x11_greedy/11047.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, K;
+int a[15];
+
+int main(){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int ans = 0;
+  cin >> N >> K;
+  for(int i=0; i<N; i++) cin >> a[i];
+  for(int i=N-1; i>=0; i--){
+    ans += K/a[i];
+    K %= a[i];
+  }
+  cout << ans;
+}

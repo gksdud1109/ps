7629ff6c23ebffad0a824a310dde2889222a0b8e
gksdud1109 <vectorh532@gmail.com>
2026-03-02 19:45:58 +0900

    연습문제:1620-나는야 포켓몬 마스터 이다솜

---
+++ 2026/0x15_hash/1620.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+unordered_map<string, int> s2i;
+string i2s[100005];
+
+int main(){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int N, M;
+  cin >> N >> M;
+  for(int i=1; i<=N; i++){
+    cin >> i2s[i];
+    s2i[i2s[i]] = i;
+  }
+  while(M--){
+    string query;
+    cin >> query;
+    if(isdigit(query[0]))
+      cout << i2s[stoi(query)] << '\n';
+    else
+      cout << s2i[query] << '\n';
+  }
+}

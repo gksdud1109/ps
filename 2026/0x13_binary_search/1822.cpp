bd79995cec0d466123256f3e5a8afd4f02bda770
gksdud1109 <vectorh532@gmail.com>
2026-03-27 00:06:22 +0900

    solve: 1822-차집합

---
+++ 2026/0x13_binary_search/1822.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int nA, nB;
+vector<int> A, B;
+
+int main(){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> nA >> nB;
+
+  int num;
+  for(int i=0; i<nA; i++){
+    cin >> num;
+    A.push_back(num);
+  }
+  for(int i=0; i<nB; i++){
+    cin >> num;
+    B.push_back(num);
+  }
+
+  sort(A.begin(), A.end());
+  sort(B.begin(), B.end());
+  vector<int> ans;
+  for(int num : A){
+    if(binary_search(B.begin(), B.end(), num)) continue;
+    ans.push_back(num);
+  }
+  cout << ans.size() << "\n";
+  for (int num : ans) cout << num << " ";
+}
